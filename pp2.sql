3be6ee24dd878ec1342778a9d9891e33e193d442
rajashiyam001-lab <rajashiyam001@gmail.com>
2026-04-27 02:35:29 +0200

    Update pp2.sql

--- pp2.sql
+++ pp2.sql
@@ -5,12 +5,12 @@ from INFORMATION_SCHEMA.TABLES
 select distinct country from [gold.dim_customers]
 
 
--- Explorer toutes les catégories et les principales divisions
+-- Explorer toutes les catÃ©gories et les principales divisions
 SELECT DISTINCT category ,subcategory,product_name from dim_products
 
--- Exploration des dates pour identifier les premières et dernières dates (bornes)
--- Comprendre l’étendue des données et la période couverte
--- Trouver la date de la première et de la dernière commande
+-- Exploration des dates pour identifier les premiÃ¨res et derniÃ¨res dates (bornes)
+-- Comprendre lâ€™Ã©tendue des donnÃ©es et la pÃ©riode couverte
+-- Trouver la date de la premiÃ¨re et de la derniÃ¨re commande
 
 SELECT 
     MIN(order_date) AS first_order_date,
@@ -18,7 +18,7 @@ SELECT
     DATEDIFF(YEAR, MIN(order_date), MAX(order_date)) AS order_range_years
 FROM dbo.fact_sales
 
--- Trouver les clients les plus jeunes et les plus âgés
+-- Trouver les clients les plus jeunes et les plus Ã¢gÃ©s
 
 select min(birthdate) as oldest_birthdate,
 max(birthdate) as youngest_birthdate,
@@ -30,7 +30,7 @@ from [gold.dim_customers]
 select sum(sales_amount) as total_sale
 from fact_sales
 
--- Calculer le nombre total d’articles vendus
+-- Calculer le nombre total dâ€™articles vendus
 select sum(quantity) as total_quantity
 from fact_sales
 
@@ -50,11 +50,11 @@ from dim_products
 select count(customer_key) as total_customer 
 from [gold.dim_customers]
 
--- Calculer le nombre total de clients ayant passé une commande
+-- Calculer le nombre total de clients ayant passÃ© une commande
 select count(distinct(customer_key)) as total_orders
 from fact_sales
 
--- Générer un rapport affichant les indicateurs clés de l’entreprise
+-- GÃ©nÃ©rer un rapport affichant les indicateurs clÃ©s de lâ€™entreprise
  
 select 'Total_sales' as measure_name , SUM(sales_amount) as measure_value
 from fact_sales
@@ -77,7 +77,7 @@ SELECT 'Total Nr. Customers' as measure_name,COUNT(customer_key) as measure_valu
 FROM [gold.dim_customers]
 
 
--- Analyse de l’ampleur (magnitude)
+-- Analyse de lâ€™ampleur (magnitude)
 -- Trouver le nombre total de clients par pays
 SELECT COUNT(customer_key) as total_customers
 FROM [gold.dim_customers]
@@ -90,19 +90,19 @@ FROM [gold.dim_customers]
 group by gender
 order by total_customers desc
 
--- Trouver le nombre total de produits par catégorie
+-- Trouver le nombre total de produits par catÃ©gorie
 select category,COUNT(product_key) as total_products
 from dim_products
 group by category 
 order by total_products desc 
 
--- Quel est le coût moyen dans chaque catégorie ?
+-- Quel est le coÃ»t moyen dans chaque catÃ©gorie ?
 select category ,AVG(cost) as avg_costs
 from dim_products
 group by category
 order by avg_costs desc
 
--- Quel est le revenu total généré par chaque catégorie ?
+-- Quel est le revenu total gÃ©nÃ©rÃ© par chaque catÃ©gorie ?
 select 
     sum(f.sales_amount) as total_revenue,
     p.category
@@ -111,7 +111,7 @@ left join dim_products p
     on p.product_key = f.product_key
 group by p.category;
 
--- Trouver le revenu total généré par chaque client
+-- Trouver le revenu total gÃ©nÃ©rÃ© par chaque client
 select c.customer_key,c.first_name,c.last_name,sum(f.sales_amount) as total_revenue
 from fact_sales f
 left join [gold.dim_customers] c 
@@ -119,7 +119,7 @@ on c.customer_key = f.customer_key
 group by c.customer_key,c.first_name,c.last_name
 order by total_revenue desc
 
--- Quelle est la répartition des articles vendus par pays ?
+-- Quelle est la rÃ©partition des articles vendus par pays ?
 select c.country,sum(f.quantity) as total_sold_items
 from fact_sales f 
 left join [gold.dim_customers] c 
@@ -127,7 +127,7 @@ on c.customer_key=f.customer_key
 group by c.country
 order by total_sold_items 
 
--- Quels sont les 5 produits générant le plus de revenus ?
+-- Quels sont les 5 produits gÃ©nÃ©rant le plus de revenus ?
 select top 5  p.product_name, sum(f.sales_amount) total_revenue
 from fact_sales f 
 left join  dim_products p
@@ -143,7 +143,7 @@ on p.product_key = f.product_key
 group by p.product_name 
 order by total_revenue asc;
 
--- Trouver les 10 clients ayant généré le plus de revenus
+-- Trouver les 10 clients ayant gÃ©nÃ©rÃ© le plus de revenus
 select c.customer_key,
 c.first_name,
 c.last_name,
@@ -154,7 +154,7 @@ on c.customer_key = f.customer_key
 group by c.customer_key ,c.first_name,c.last_name
 order by total_revenue desc 
 
--- Les 10 clients ayant passé le moins de commandes
+-- Les 10 clients ayant passÃ© le moins de commandes
 select top 10 c.customer_key,
 c.first_name,
 c.last_name,
@@ -164,3 +164,106 @@ left join [gold.dim_customers] c
 on c.customer_key = f.customer_key
 group by c.customer_key ,c.first_name,c.last_name
 order by total_orders
+
+
+--advanced analytics 
+
+--changes over the time analysis (par mois)
+--its for analysis the trends and identigiy seasonitly in this data
+SELECT 
+    month(order_date) AS order_year,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY month(order_date)
+ORDER BY order_year;
+
+--anaylysis cumlative 
+--aggreate the data progrsssively oiber time helps to undertsand whether our buisnnessi growing or decling 
+--Cumulative (Running Total) Analysis
+--Why:To understand the progression and growth of the business by adding values progressively over time rather than looking at individual periods in isolation.
+SELECT 
+    order_date,
+    total_sales,
+    SUM(total_sales) OVER (ORDER BY order_date) AS running_total_sales
+FROM (
+    SELECT DATETRUNC(month, order_date) AS order_date, SUM(sales_amount) AS total_sales
+    FROM fact_sales
+    GROUP BY DATETRUNC(month, order_date)
+) t;
+---Performance Analysis (Year-over-Year)
+--: To compare current performance against a target or a previous period 
+--(e.g., comparing this year's sales to last year's) to measure success
+
+
+--Part-to-Whole Analysis
+--Why: To determine the proportion of a category relative to the entire business,
+--helping to identify the most impactful segments.
+
+WITH yearly_product_sales AS (
+    SELECT 
+        YEAR(f.order_date) AS order_year,
+        p.product_name,
+        SUM(f.sales_amount) AS current_sales
+    FROM fact_sales AS f
+    LEFT JOIN dim_products AS p 
+        ON f.product_key = p.product_key
+    WHERE f.order_date IS NOT NULL
+    GROUP BY 
+        YEAR(f.order_date), 
+        p.product_name
+),
+
+sales_with_prev AS (
+    SELECT
+        order_year,
+        product_name,
+        current_sales,
+        LAG(current_sales) OVER (
+            PARTITION BY product_name 
+            ORDER BY order_year
+        ) AS previous_year_sales
+    FROM yearly_product_sales
+)
+
+SELECT
+    order_year,
+    product_name,
+    current_sales,
+    previous_year_sales,
+    current_sales - previous_year_sales AS diff_prev_year,
+    CASE
+        WHEN previous_year_sales IS NULL THEN 'No Previous Data'
+        WHEN current_sales > previous_year_sales THEN 'Increase'
+        WHEN current_sales < previous_year_sales THEN 'Decrease'
+        ELSE 'No Change'
+    END AS py_change
+FROM sales_with_prev
+ORDER BY product_name, order_year;
+
+--Data Segmentation (Customer)
+--This segments customers into groups based on two metrics
+--: Lifespan (how long they've been with the company) and Total Spending.
+--Logic: * VIP: At least 12 months history AND > $5,000 spent
+--Regular: At least 12 months history AND = $5,000 spent
+WITH customer_spending AS (
+    SELECT 
+        customer_key,
+        SUM(sales_amount) AS total_spending,
+        DATEDIFF(MONTH, MIN(order_date), MAX(order_date)) AS lifespan
+    FROM gold.fact_sales
+    GROUP BY customer_key
+)
+SELECT 
+    customer_key,
+    total_spending,
+    lifespan,
+    -- Create Segments
+    CASE 
+        WHEN lifespan >= 12 AND total_spending > 5000 THEN 'VIP'
+        WHEN lifespan >= 12 AND total_spending <= 5000 THEN 'Regular'
+        ELSE 'New'
+    END AS customer_segment
+FROM customer_spending;
